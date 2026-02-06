49d1e8d233ea6fa78ab72eba647e9460d37412e7
RiddhiVIT <riddhi.bhaduri2024@vitstudent.ac.in>
2026-02-07 04:14:15 +0530

    cleaned up page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import { ComponentExample } from "@/components/component-example";
 import Navbar from "@/components/navbar";
 import Dashboard from "@/pages/dashboard";
-import Signup from "@/components/sign-up";
-import Login from "@/components/login";
 
 export default function Page() {
   return (
     <div>
-      <Login />
     </div>
 
   );
