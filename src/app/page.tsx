d985dbc01bf5ad3804690d2e84cf7b232a8bfae1
RiddhiVIT <riddhi.bhaduri2024@vitstudent.ac.in>
2026-02-07 04:11:56 +0530

    made ui for sign up and login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { ComponentExample } from "@/components/component-example";
 import Navbar from "@/components/navbar";
 import Dashboard from "@/pages/dashboard";
+import Signup from "@/components/sign-up";
+import Login from "@/components/login";
 
 export default function Page() {
   return (
-    <div></div>
+    <div>
+      <Login />
+    </div>
 
   );
 }
